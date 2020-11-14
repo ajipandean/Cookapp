b94fd7f4f836135e8732e8c449ad3889e5b2d8b2
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-11-14 23:37:48 +0800

    remove explore and favorites screen

--- screens/Main/index.js
+++ screens/Main/index.js
@@ -1,6 +1,4 @@
 import HomeScreen from './Home';
-import ExploreScreen from './Explore';
-import FavoriteScreen from './Favorite';
 import ProfileScreen from './Profile';
 
 module.exports = [
@@ -8,16 +6,6 @@ module.exports = [
     name: 'Home',
     icon: 'home',
     component: HomeScreen,
-  },
-  {
-    name: 'Explore',
-    icon: 'compass',
-    component: ExploreScreen,
-  },
-  {
-    name: 'Favorites',
-    icon: 'heart',
-    component: FavoriteScreen,
   },
   {
     name: 'Profile',
